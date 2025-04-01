540141f39fcf19aeedbda40e03579946510ac202
Ian Sang <sangian006@gmail.com>
2025-04-01 12:58:01 +0300

    completes stitching together the three pillars

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 /* This is a file inserted to keep the learn IDE browser happy */
-const testVar = {}
+const testVar = {};
+// Step 1: Select the heart elements
+const articleHearts = document.querySelectorAll(".like-glyph");
+// Step 2: Mock server communication
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Success!");
+    }, 300);
+  });
+}
+// Step 3: Add event listeners
+articleHearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        heart.classList.toggle("activated-heart");
+      })
+      .catch((error) => {
+        alert("Server error. Please try again.");
+      });
+  });
+});
